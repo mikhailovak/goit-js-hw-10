95138b9c4b205ebf2877b7ebccbbb0d4e4d55b5c
Ksenia Melnyk <melnyksenia@gmail.com>
2024-11-03 14:17:17 +0100

    add form

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,52 @@
-console.log("hello1");
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", OnSubmitClick);
+
+function delayPromise(delay, radioChecked) {
+    const data = { delay, radioChecked };
+
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            if (radioChecked === 'fulfilled') {
+                res(data); 
+            } else {
+                rej(data);
+            }
+        }, delay);
+    });
+}
+
+
+function OnSubmitClick(event) {
+    event.preventDefault(); 
+
+    const form = event.target;
+    const delay = +(form.elements.delay.value); 
+    const radioChecked = form.elements.state.value; 
+
+    delayPromise(delay, radioChecked)
+        .then(({ delay }) => {
+            iziToast.success({
+                title: 'OK',
+                message: `✅ Fulfilled promise in ${delay} ms`,
+                backgroundColor: "#4caf50", 
+                position: 'topRight',
+                timeout: 5000
+            });
+        })
+        .catch(({ delay }) => {
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${delay} ms`,
+                backgroundColor: "#ef4040",
+                position: 'topRight',
+                timeout: 5000
+            });
+        });
+
+
+    form.reset();
+}
